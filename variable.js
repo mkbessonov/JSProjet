29db6dddec580fb0a5abea5a52a4b20fc6eaa4c3
mkbessonov <mkbessonov@mail.ru>
2020-05-06 15:02:15 +0300

    variable.js модифицирован

--- variable.js
+++ variable.js
@@ -1,6 +1,33 @@
 "use strict"
-function localVariable(){
+
+function localVariable() {
     let message = "Hello world!";
     console.log(message);
+    let $ = 1;                  //Так можно
+    let _ = 2;                  //Так тоже можно
+    console.log($);
+    console.log(_);
+
+    //let 1a = 5;               //не может начинаться с цифры
+    //let my-name;              //- не разрешен
+
+    let apple = "яблоко";
+    let Apple = "тыква";        //Регистр имеет значение
+    console.log(apple);
+    console.log(Apple);
+
+    let яблоко = "Яблоко";      //Нелатинские буквы можно, но не рекомендуется.
+    console.log(яблоко);
+
+    //num = 5;                  //можно без use strict
+
+    const myBirthday = "01.01.1990";            //константа
+    console.log(myBirthday);
+    //myBirthday = "02.01.1990";                //Ошибка
+    //константы с именами, записанными заглавными буквами,
+    // используются только как псевдонимы для «жёстко закодированных» значений
+    const COLOR_BLUE = "#00F";
+    console.log(COLOR_BLUE);
 }
+
 localVariable();
